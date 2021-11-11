d5cdc6e5a52b1cf2766078525e63f0619026e2a5
Arhey- <Arhey-@users.noreply.github.com>
2021-11-12 01:15:10 +0200

    html.js

---
+++ html.js
@@ -0,0 +1,34 @@
+function createElement(name, opts = {}, ...children) {
+  const el = document.createElement(name)
+  el.append(...children)
+  if (typeof opts == 'function' && 'button' == name) {
+    opts = { onclick: opts }
+  }
+  return Object.assign(el,opts)
+}
+
+export const html = new Proxy (createElement, {
+  get: (t, name) => (props, ...childs) => typeof props == 'string' || props instanceof Node
+    ? createElement(name, null, props, ...childs)
+    : createElement(name, props, ...childs)
+})
+
+export function select(opts){
+  const s=html.select()
+  for(const v of opts)
+    s.append(option(v))
+  return s
+}
+
+function option(value, text = value) {
+  if (Array.isArray(value)) {
+    [value, text] = value
+  }
+  return html.option({ value }, text)
+}
+
+export function fold(title, ...details) {
+  const d = html.details()
+  d.append(html.summary(title), ...details)
+  return d
+}
