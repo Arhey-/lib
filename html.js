ffb334ab93f1485f09c0a48d9e474b2f0f6718f8
Arhey- <Arhey-@users.noreply.github.com>
2023-01-14 18:31:41 +0200

    allow HTMLCollection in children

--- html.js
+++ html.js
@@ -1,14 +1,18 @@
 function createElement(name, opts = {}, ...children) {
   const el = document.createElement(name)
-  el.append(...children)
+  el.append(...children.flatMap(i => i instanceof HTMLCollection ? Array.from(i) : i))
   if (typeof opts == 'function' && 'button' == name) {
     opts = { onclick: opts }
   }
   return Object.assign(el,opts)
 }
 
-export const html = new Proxy (createElement, {
-  get: (t, name) => (props, ...childs) => typeof props == 'string' || props instanceof Node
+export const html = new Proxy(createElement, {
+  get: (t, name) => (props, ...childs) => (
+    typeof props == 'string'
+    || props instanceof Node
+    || props instanceof HTMLCollection
+  )
     ? createElement(name, null, props, ...childs)
     : createElement(name, props, ...childs)
 })
