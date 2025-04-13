8308995b14bf42028c4da6cdbe73873d6f14021c
Arhey- <Arhey-@users.noreply.github.com>
2025-04-13 03:26:12 +0300

    reactive: batch

--- reactive.js
+++ reactive.js
@@ -1,19 +1,39 @@
-function reactive(value) {
-  let subs = [];
+let isBatching = false;
+const batching = new Map();
+
+export function batch(fn) {
+  isBatching = true;
+  fn();
+  isBatching = false;
+  for(const [cb, value] of batching) {
+    cb(value)
+  }
+  batching.clear()
+}
+
+export function reactive(value) {
+  const subs = [];
 
   function rx(v) {
     if (!arguments.length) return value;
     if (v === value) return;
     value = v;
-    subs.forEach(cb => cb(value))
+    for (const cb of subs) {
+      if (isBatching) {
+        batching.set(cb, value)
+      } else {
+        cb(value)
+      }
+    }
   }
 
   rx.dispose = () => subs.splice(0);
   rx.watch = cb => subs.push(cb);
   rx.map = (fn, ...deps) => {
     const m = reactive(fn(value, ...deps.map(d => d())))
+    const w = () => m(fn(value, ...deps.map(d => d())))
     for (const d of [rx, ...deps]) {
-      d.watch(() => m(fn(value, ...deps.map(d => d()))))
+      d.watch(w)
     }
     return m
   };
